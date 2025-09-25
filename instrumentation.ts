83877a7b947fdf3dd5e7618bda5df40d7249e7c1
Hansam-Yang <yanga6257@gmail.com>
2025-09-25 09:41:51 +0900

    [양한샘] 스케줄 실행 주석 처리

--- instrumentation.ts
+++ instrumentation.ts
@@ -8,12 +8,11 @@ export async function register() {
     const cron = await import('node-cron');
     console.log('⏰ Scheduler has been initialized.');
 
-  //   // 매일 자정에 실행되는 스케쥴을 설정
-  //   // Cron 표현식: '분 시 일 월 요일'
-  //   // 예: '0 0 * * *' -> 매일 0시 0분에 실행
-    cron.schedule('30 * * * *', () => {
-      console.log('🚀 Running a scheduled job at midnight...');
-
+    // 매일 자정에 실행되는 스케쥴을 설정
+    // Cron 표현식: '분 시 일 월 요일'
+    // 예: '0 0 * * *' -> 매일 0시 0분에 실행
+    // cron.schedule('30 * * * *', () => {
+    //   console.log('🚀 Running a scheduled job at midnight...');
     //   fetch(`${siteUrl}/api/scheduler/send`)
     //     .then(res => {
     //       if (!res.ok) {
